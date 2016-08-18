2656038d9bdf86585cc3ef634cea8f24a77e80c0
binaywebcorridor <webcorridor@webs-mac-mini.local>
2016-08-18 18:51:25 +0530

    try

---
+++ SmashTag/TextViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TextViewController.swift
+//  SmashTag
+//
+//  Created by web corridor on 27/05/1938 Saka.
+//  Copyright © 1938 Saka web corridor. All rights reserved.
+//
+
+import UIKit
+
+class TextViewController: UIViewController {
+    
+    var blogName = String()
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(blogName)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
